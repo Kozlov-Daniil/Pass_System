9b5d114740f411756b1fbb49e331316a767149ec
Biruda17 <57209235+Biruda17@users.noreply.github.com>
2021-12-09 01:17:57 +0300

    Update check.php

--- site/check.php
+++ site/check.php
@@ -3,7 +3,7 @@
     $phone_number = filter_var(trim($_POST['phone_number']), FILTER_SANITIZE_STRING);
     $id_user = filter_var(trim($_POST['id_user']), FILTER_SANITIZE_STRING);
 
-    $mysql = new mysqli('localhost','root','','passes');
+    $mysql = new mysqli('localhost','root','','pass_system');
     $mysql->query("INSERT INTO `users` (`name`, `phone_number`, `id_user`) VALUES ('$name', '$phone_number', '$id_user')");
 
     $mysql->close();
