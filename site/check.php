f18d69c47e57ef72c314d21f3235d4f1ff7d5f8e
Biruda17 <57209235+Biruda17@users.noreply.github.com>
2021-12-13 19:14:38 +0300

    Add files via upload

--- site/check.php
+++ site/check.php
@@ -1,12 +1,14 @@
 <?php
     $name = filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
     $phone_number = filter_var(trim($_POST['phone_number']), FILTER_SANITIZE_STRING);
-    $id_user = filter_var(trim($_POST['id_user']), FILTER_SANITIZE_STRING);
+    $lot_number = filter_var(trim($_POST['lot_number']), FILTER_SANITIZE_STRING);
+    $user_id = filter_var(trim($_POST['user_id']), FILTER_SANITIZE_STRING);
+
 
     $mysql = new mysqli('localhost','root','','pass_system');
-    $mysql->query("INSERT INTO `users` (`name`, `phone_number`, `id_user`) VALUES ('$name', '$phone_number', '$id_user')");
+    $mysql->query("INSERT INTO `users` (`name`, `phone_number`, `lot_number`, `user_id`) VALUES ('$name', '$phone_number', '$lot_number', '$user_id')");
 
     $mysql->close();
 
-    header('Location: /Passes/index.php');
+    header('Location: index.php');
 ?>
