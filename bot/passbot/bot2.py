38d3682a098c6f2416bc036815448defb9124309
Kozlov-Daniil <57208983+Kozlov-Daniil@users.noreply.github.com>
2021-12-13 04:31:50 +0300

    Update bot2.py
    Вырезано всё, что не требовалось на данном этапе разработки
    Клавиатура переделана с обычной на инлайновую, теперь при нажатии на кнопки меню надпись в чат не дублируется
    При нажатии на кнопку регистрации происходит та же процедура что и прежде: ввод ФИО, телефона, номера участка; после чего пользователь заносится в БД либо если его данные уже занесены выводится соответствующее уведомление
    После чего выводится сообщение "Меню" с прикреплёнными к нему кнопками

--- bot/passbot/bot2.py
+++ bot/passbot/bot2.py
@@ -44,23 +44,29 @@ class Car:
 		self.phonenumbers = ''
 		self.comment = ''
 
+
+
 @bot.message_handler(commands=['start'])
 def start (message):
-	markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-	item1 = types.KeyboardButton('/Регистрация')
-	item2 = types.KeyboardButton('/Авторизация')
-	item3 = types.KeyboardButton('/Сайт')
 
-	markup.add(item1, item2, item3)
+	markup_inline = types.InlineKeyboardMarkup()
+	item_1 = types.InlineKeyboardButton(text = 'Регистрация', callback_data = '1')
+	item_2 = types.InlineKeyboardButton(text = 'Подача заявки', callback_data = '2')
+	item_3 = types.InlineKeyboardButton(text = 'Сайт', callback_data = '3')
 
-	bot.send_message(message.chat.id, 'Здравствуйте, {0.first_name}, для возможности подачи заявки вам необходимо авторизироваться!'.format(message.from_user), reply_markup = markup)
-
-
-@bot.message_handler(commands=['Регистрация', 'help'])
-def send_welcome(message):
-	msg = bot.send_message(message.chat.id, "Введите ФИО")
-	bot.register_next_step_handler(msg, process_name_step)
-
+	markup_inline.add(item_1, item_2, item_3)
+	bot.send_message(message.chat.id, 'Для подачи заявки вам необходимо зарегистрироваться.',
+	reply_markup = markup_inline
+	)
+@bot.callback_query_handler(func = lambda call: True)
+def answer(call):
+	if call.data == '1':
+		msg = bot.send_message(call.message.chat.id, "Введите ФИО")
+		bot.register_next_step_handler(msg, process_name_step)
+	elif call.data == '2':
+		msg = bot.send_message(call.message.chat.id, "В разработке...")
+	else:
+		msg = bot.send_message(call.message.chat.id, "Ссылка на сайт:")
 def process_name_step(message):
 	try:
 		user_id = message.from_user.id
@@ -96,60 +102,19 @@ def process_lotnumber_step(message):
 
 
 		bot.send_message(message.chat.id, "Регистрация завершена")
-		markup = types.ReplyKeyboardMarkup(resize_keyboard = True, one_time_keyboard=True)
-		item1 = types.KeyboardButton('/Регистрация')
-		item2 = types.KeyboardButton('/Авторизация')
-		item3 = types.KeyboardButton('/Сайт')
-
-		markup.add(item1, item2, item3)
-
-
+		
 	except Exception as e:
 		bot.reply_to(message, 'Ошибка или вы уже зарегистрированы')
 
-	
+	markup_inline = types.InlineKeyboardMarkup()
+	item_1 = types.InlineKeyboardButton(text = 'Регистрация', callback_data = '1')
+	item_2 = types.InlineKeyboardButton(text = 'Подача заявки', callback_data = '2')
+	item_3 = types.InlineKeyboardButton(text = 'Сайт', callback_data = '3')
 
-
-@bot.message_handler(commands=['Авторизация'])
-def auth(message):
-	msg = bot.send_message(message.chat.id, "Введите логин")
-	bot.register_next_step_handler(msg, process_login_step)
-def process_login_step(message):
-		user_login = message.text
-		user_id = message.from_user.id
-		user_data[user_id] = User(message.text)
-		sql = "INSERT INTO user (user_id) VALUES (%s)"
-		val = (user_id)
-		sql = "SELECT * FROM users WHERE user_id = {0}".format(user_id)
-		cursor.execute(sql)
-		existsUser = cursor.fetchone()
-		markup_inline = types.InlineKeyboardMarkup()
-		item_yes = types.InlineKeyboardButton(text = 'ДА', callback_data = 'yes')
-		item_no = types.InlineKeyboardButton(text = 'Отмена', callback_data = 'no')
-
-		markup_inline.add(item_yes, item_no)
-		bot.send_message(message.chat.id, 'Желаете получить доступ к подаче заявок?',
-		reply_markup = markup_inline
-		)
-@bot.callback_query_handler(func = lambda call: True)
-def answer(call):
-	if call.data == 'yes':
-		markup_reply = types.ReplyKeyboardMarkup(resize_keyboard = True)
-		item1 = types.KeyboardButton('/Регистрация')
-		item2 = types.KeyboardButton('/Авторизация')
-		item3 = types.KeyboardButton('/Сайт')
-		item4 = types.KeyboardButton('/Подача заявки')
-
-		markup_reply.add(item1, item2, item3, item4)
-		bot.send_message(call.message.chat.id, 'Авторизация завершена, доступ к заявкам получен',
-			reply_markup = markup_reply
-			)
-	elif call.data == 'no':
-		pass
-
-@bot.message_handler(commands=['Сайт'])
-def site(message):
-	bot.reply_to(message, 'Ссылка на сайт:')
+	markup_inline.add(item_1, item_2, item_3)
+	bot.send_message(message.chat.id, 'Меню',
+	reply_markup = markup_inline
+	)
 
 
 
