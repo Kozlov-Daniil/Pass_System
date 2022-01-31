595bfaf753c27ba3d67f7941b14f9fe9dc5775cd
Biruda17 <57209235+Biruda17@users.noreply.github.com>
2022-01-31 03:10:39 +0300

    Delete deleteuser.php

--- deleteuser.php
+++
@@ -1,9 +0,0 @@
-<?php
-     $mysql = new mysqli('localhost','root','','pass_system');
-
-     $id_user = $_GET['id_user'];
-
-     mysqli_query($mysql, "DELETE FROM `users` WHERE `users`.`id_user` = '$id_user'");
-
-     header('Location: allusers.php')
-?>
